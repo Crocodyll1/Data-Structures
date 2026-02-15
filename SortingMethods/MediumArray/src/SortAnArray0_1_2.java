65cd82405db8755e4953516c3f8fb1de23d8351f
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-02-15 23:58:04 +0530

    sort an array 0,1,2

---
+++ SortingMethods/MediumArray/src/SortAnArray0_1_2.java
@@ -0,0 +1,25 @@
+public class SortAnArray0_1_2 {
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 2, 1, 1, 0};
+        int n = arr.length;
+
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+
+                if(arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+
+
+
+    }
+}
