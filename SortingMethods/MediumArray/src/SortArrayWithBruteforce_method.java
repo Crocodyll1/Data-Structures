2b1e9a7233131f9c011c01ba0d5dfc9f23348687
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-02-16 14:31:56 +0530

    Sorting of 0,1,2 arrays with bruteforce method

--- SortingMethods/MediumArray/src/SortArrayWithBruteforce_method.java
+++ SortingMethods/MediumArray/src/SortArrayWithBruteforce_method.java
@@ -1,2 +1,46 @@
 public class SortArrayWithBruteforce_method {
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 2, 1, 1, 0};
+        int n = arr.length;
+        int cout0 = 0;
+        int cout1 =0;
+        int cout2 = 0;
+
+        for(int i =0; i<n; i++){
+            if(arr[i] == 0){
+                cout0++;
+            }
+            else if(arr[i] == 1){
+                cout1++;
+            }
+            else{
+                cout2++;
+            }
+        }
+        for(int i =0; i<cout0; i++){
+            arr[i] = 0;
+        }
+        for(int i = cout0; i <cout0+cout1; i++){
+            arr[i] = 1;
+        }
+        for(int i =cout0+cout1; i<n; i++){
+            arr[i] = 2;
+        }
+
+        for(int val : arr){
+            System.out.println(val+ " ");
+        }
+
+    }
 }
+
+
+/*
+  Sort an Array of 0,1,2
+  brutefore:
+
+  store values of 0,1,2 in variables and put the variable values in arr;
+  now print
+
+  TC = O(N)
+ */
