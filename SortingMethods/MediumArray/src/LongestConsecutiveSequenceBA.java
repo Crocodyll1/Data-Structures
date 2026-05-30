b57d2edc3b1ee337c8e6fc0352a797998e2ed996
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-05-30 23:59:58 +0530

    i

--- SortingMethods/MediumArray/src/LongestConsecutiveSequenceBA.java
+++ SortingMethods/MediumArray/src/LongestConsecutiveSequenceBA.java
@@ -18,7 +18,7 @@ public class LongestConsecutiveSequenceBA {
                 continue;
             }
             if(nums[i] == nums[i-1]+1){
-                count++;
+                count += 1;
 
             }else{
                 count = 1;
