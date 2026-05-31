da26e8c1d45a0b4f678f5ac2224e56bd6fb07e10
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-05-31 21:04:38 +0530

    SetMatrixZeroes

---
+++ SortingMethods/MediumArray/src/SetMatrixZeroesBA.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class SetMatrixZeroesBA {
+
+    public static void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] row = new int[n];
+        int[] col = new int[m];
+
+        // Mark rows and columns
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Set zeroes
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        setZeroes(matrix);
+
+        System.out.println("\nAfter Set Matrix Zeroes:");
+        printMatrix(matrix);
+    }
+}
