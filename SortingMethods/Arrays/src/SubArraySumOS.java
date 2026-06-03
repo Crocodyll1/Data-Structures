968649b021b725c47ac315abdfd3dd4b9e5f243f
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-06-03 23:39:53 +0530

    Sum of SubArray  Optiml Solution

---
+++ SortingMethods/Arrays/src/SubArraySumOS.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class SubArraySumOS {
+
+
+        public static int subarraySum(int[] nums, int k) {
+
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+
+            int sum = 0;
+            int count = 0;
+
+            for (int num : nums) {
+                sum += num;
+
+                count += map.getOrDefault(sum - k, 0);
+
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+
+            return count;
+        }
+
+        public static void main(String[] args) {
+
+            int[] nums = {1, 1, 1};
+            int k = 2;
+
+            System.out.println(subarraySum(nums, k));
+        }
+    }
+
