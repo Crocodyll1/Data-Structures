ea4aeff53fb9967684c334a10ef496a592376acb
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-06-14 23:50:58 +0530

    BF

---
+++ SortingMethods/Hard/src/MergeOverLapingIntervalsBF.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+class MergeOverLapingIntervalsBF {
+    public int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            // Skip if already merged
+            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1)[1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (intervals[j][0] <= end) {
+                    end = Math.max(end, intervals[j][1]);
+                } else {
+                    break;
+                }
+            }
+
+            ans.add(new int[]{start, end});
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
