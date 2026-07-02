5b436ab5db05795703eba87fb555205bd0d86ca0
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-07-02 23:35:50 +0530

    Time: 1 ms (56.1%), Space: 43.2 MB (32.65%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,43 +1,48 @@
 class Solution {
-
-    public int splitArray(int[] nums, int k) {
-
-        int low = 0, high = 0;
-
-        for (int num : nums) {
-            low = Math.max(low, num);
-            high += num;
-        }
-
-        while (low < high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (canSplit(nums, k, mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
+    
+    public boolean books(int[] nums, int maxPages, int students){
+        int student = 1;
+        int pages = 0;
+        
+        for(int i =0; i<nums.length; i++){
+            if(nums[i] + pages <= maxPages ){
+                pages += nums[i];
+            }
+            else{
+                student ++;
+                pages = nums[i];
             }
         }
-
-        return low;
+        if(student > students){
+            return false;
+        }
+        return true;
     }
-
-    private boolean canSplit(int[] nums, int k, int maxSum) {
-
-        int count = 1;
+    public int splitArray(int[] arr, int k) {
+        // code here
+        int high = 0;
         int sum = 0;
-
-        for (int num : nums) {
-
-            if (sum + num <= maxSum) {
-                sum += num;
-            } else {
-                count++;
-                sum = num;
+        int ans = 0;
+        
+        for(int num : arr){
+            sum += num;
+            high = Math.max(high, num);
+        }
+        if(k > arr.length){
+            return -1;
+        }
+        
+        while( high <= sum){
+            int mid = high + (sum - high)/2;
+            
+            if(books(arr, mid, k)){
+                ans = mid;
+                sum = mid -1;
+            }
+            else{
+                high = mid + 1;
             }
         }
-
-        return count <= k;
+        return ans;
     }
 }
