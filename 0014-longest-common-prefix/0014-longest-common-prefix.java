e72badef3de6a67b3081eacbe294432900e36d39
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-07-14 15:33:45 +0530

    Time: 2 ms (17.42%), Space: 43.3 MB (51.15%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -15,12 +15,17 @@ class Solution {
     }
     public String longestCommonPrefix(String[] strs) {
 
-        
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
         String res = strs[0];
 
         for(int i =1; i< strs.length; i++){
             res = common(res, strs[i]);
         }
+        if (res.isEmpty()) {   // Early exit
+                return "";
+            }
         return res.toString();
        
     }
