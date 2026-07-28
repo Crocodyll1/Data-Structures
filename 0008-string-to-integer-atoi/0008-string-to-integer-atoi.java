1f18069700c31dbe448ddca8cb788751f0c0a47c
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-07-28 19:36:24 +0530

    Time: 1 ms (100%), Space: 43.9 MB (41.99%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,41 +1,27 @@
 class Solution {
     public int myAtoi(String s) {
-
-        int i = 0;
+        int i =0;
         int n = s.length();
-
-        // Skip leading spaces
-        while (i < n && s.charAt(i) == ' ') {
+        while(i<n && s.charAt(i) == ' '){
             i++;
         }
-
-        // Sign
         int sign = 1;
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            if (s.charAt(i) == '-') {
+        if(i<n && (s.charAt(i) == '-' || s.charAt(i) == '+')){
+            if(s.charAt(i) == '-'){
                 sign = -1;
             }
             i++;
         }
-
         int result = 0;
-
-        // Read digits
-        while (i < n && Character.isDigit(s.charAt(i))) {
-
-            int digit = s.charAt(i) - '0';
-
-            // Overflow check
-            if (result > (Integer.MAX_VALUE - digit) / 10) {
-                return sign == 1
-                        ? Integer.MAX_VALUE
-                        : Integer.MIN_VALUE;
+        while(i<n && Character.isDigit(s.charAt(i))){
+            int res = s.charAt(i) - '0';
+            if(result > (Integer.MAX_VALUE - res)/10){
+                return sign ==1 ?Integer.MAX_VALUE: Integer.MIN_VALUE;
             }
-
-            result = result * 10 + digit;
+            result = result * 10 + res;
             i++;
         }
+        return sign * result;
 
-        return result * sign;
     }
 }
